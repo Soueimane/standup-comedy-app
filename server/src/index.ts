975c838bb039ed2c74af8b9d723a0fd4b2f78aff
dahmane7 <bilelzineb@gmail.com>
2025-11-20 13:41:03 +0100

    Fix: Augmenter limite JSON pour upload avatar

--- server/src/index.ts
+++ server/src/index.ts
@@ -48,7 +48,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 // Gestion explicite des requêtes OPTIONS (preflight)
 app.options('*', cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '6mb' }));
+app.use(express.urlencoded({ extended: true, limit: '6mb' }));
 
 // Middleware de logging global pour toutes les requêtes
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
